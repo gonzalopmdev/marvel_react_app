3ee8a86a8460557b0458a361d0abe48d24c4c141
Gonzalo Palma <gonzalopm3d@gmail.com>
2022-09-10 20:19:28 +0200

    full first commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import "./App.css";
+import "semantic-ui-css/semantic.min.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./pages/Home";
+import Series from "./pages/Series";
+import Comics from "./pages/Comics";
+import Error404 from "./pages/Error404";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={() => <Home />} />
+          <Route path="/inicio" exact component={() => <Home />} />
+          <Route path="/series" exact component={() => <Series />} />
+          <Route path="/comics" exact component={() => <Comics />} />
+          <Route component={() => <Error404 />} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
